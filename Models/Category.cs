9d70e82adbfbe117ea6d6ca0edae062dd659c9aa
Dung Tran <tn.tiendung03@gmail.com>
2026-09-03 15:32:41 +0700

    feat: add Category model

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyKho.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        [Display(Name = "Tên danh mục")]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(250)]
+        [Display(Name = "Mô tả")]
+        public string? Description { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
