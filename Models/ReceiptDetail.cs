b67bf5f77ea7e7c9048ab1e1fe3a8e730a7d4640
Dung Tran <tn.tiendung03@gmail.com>
2026-09-03 15:41:18 +0700

    feat: add ReceiptDetail Model

---
+++ Models/ReceiptDetail.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuanLyKho.Models
+{
+    public class ReceiptDetail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ReceiptId { get; set; }
+
+        [ForeignKey(nameof(ReceiptDetail.Id))]
+        public virtual Receipt? Receipt { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product? Product { get; set; }
+
+        [Required]
+        [Display(Name = "Số lượng nhập")]
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        [Display(Name = "Giá nhập")]
+        public decimal ImportPrice { get; set; }  
+    }
+}
