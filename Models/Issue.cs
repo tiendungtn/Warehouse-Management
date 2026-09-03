4da0204adde1c6f72cdd8420ed29fe1939a367f7
Dung Tran <tn.tiendung03@gmail.com>
2026-09-03 15:37:10 +0700

    feat: add Issue Model

---
+++ Models/Issue.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyKho.Models
+{
+    public class Issue
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        [Display(Name = "Mã phiếu xuất")]
+        public string IssueCode { get; set; } = string.Empty;
+
+        [MaxLength(255)]
+        [Display(Name = "Lý do xuất")]
+        public string Reason { get; set; } = string.Empty;
+
+        [Required]
+        public int CreatedBy { get; set; }
+
+        public virtual User? Creator { get; set; }
+
+        public DateTime IssueDate { get; set; } = DateTime.UtcNow;
+
+        [Required, MaxLength(30)]
+        [Display(Name = "Trạng thái")]
+        public string Status { get; set; } = "Chờ duyệt";
+
+        public virtual ICollection<IssueDetail> IssueDetails { get; set; } = new List<IssueDetail>();
+        public virtual Invoice? Invoice { get; set; }
+    }
+}
