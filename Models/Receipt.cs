dfb398ac815c76d7e60f5b2f4254580a17e08191
Dung Tran <tn.tiendung03@gmail.com>
2026-09-03 15:40:18 +0700

    feat: add Receipt Model

---
+++ Models/Receipt.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuanLyKho.Models
+{
+    public class Receipt
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        [Display(Name = "Mã phiếu nhập")]
+        public string ReceiptCode { get; set; } = string.Empty;
+
+        [Required, MaxLength(200)]
+        [Display(Name = "Tên nhà cung cấp")]
+        public string SupplierName { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Nhân viên lập")]
+        public int CreatedBy { get; set; }
+
+        [ForeignKey(nameof(CreatedBy))]
+        public virtual User? CreatedTor { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required, MaxLength(30)]
+        [Display(Name = "Trạng thái")]
+        public string Status { get; set; } = "Chờ duyệt";
+
+        public virtual ICollection<ReceiptDetail> ReceiptDetails { get; set; } = new List<ReceiptDetail>();
+    }
+}
