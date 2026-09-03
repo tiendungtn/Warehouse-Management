397f73fdef418ed055552ca232dc29fec7d2f0fa
Dung Tran <tn.tiendung03@gmail.com>
2026-09-03 15:38:15 +0700

    feat: add IssueDetail Model

---
+++ Models/IssueDetail.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuanLyKho.Models
+{
+    public class IssueDetail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int IssueId { get; set; }
+
+        [ForeignKey(nameof(IssueId))]
+        public virtual Issue? Issue { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+        
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product? Product { get; set; }
+
+        [Required]
+        [Display(Name = "Số lượng xuất")]
+        public int Quantity { get; set; }
+    }
+}
