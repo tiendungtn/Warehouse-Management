411b40d52cc0dbedaf1cd019ca9d5b80b481bb47
Dung Tran <tn.tiendung03@gmail.com>
2026-09-03 15:30:38 +0700

    ref: update User Model

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,27 @@
-﻿namespace QuanLyKho.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuanLyKho.Models
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(50)]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+
+        // Các quyền của người dùng, ví dụ: "Admin", "Staff", "Manager"
+        [Required, MaxLength(20)]
+        public string Role { get; set; } = "Staff";
+          
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // navigation property
+        public virtual ICollection<Receipt> Receipts { get; set; } = new List<Receipt>();
+        public virtual ICollection<Issue> Issues { get; set; } = new List<Issue>();
     }
 }
