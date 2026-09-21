61cb482329ffbc4857204205998789c99f0bdd45
tiendungtn <tn.tiendung03@gmail.com>
2026-09-21 15:03:51 +0700

    refactor: prepare Web API migration

--- Models/ReceiptDetail.cs
+++ Models/ReceiptDetail.cs
@@ -1,31 +1,3 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace QuanLyKho.Models
-{
-    public class ReceiptDetail
-    {
-        [Key]
-        public int Id { get; set; }
-
-        [Required]
-        public int ReceiptId { get; set; }
-
-        [ForeignKey(nameof(ReceiptDetail.Id))]
-        public virtual Receipt? Receipt { get; set; }
-
-        [Required]
-        public int ProductId { get; set; }
-
-        [ForeignKey(nameof(ProductId))]
-        public virtual Product? Product { get; set; }
-
-        [Required]
-        [Display(Name = "Số lượng nhập")]
-        public int Quantity { get; set; }
-
-        [Column(TypeName = "decimal(18,2)")]
-        [Display(Name = "Giá nhập")]
-        public decimal ImportPrice { get; set; }  
-    }
-}
+using System.ComponentModel.DataAnnotations;using System.ComponentModel.DataAnnotations.Schema;
+namespace QuanLyKho.Models;
+public class ReceiptDetail{[Key]public int Id{get;set;}[Required]public int ReceiptId{get;set;}[ForeignKey(nameof(ReceiptId))]public virtual Receipt? Receipt{get;set;}[Required]public int ProductId{get;set;}[ForeignKey(nameof(ProductId))]public virtual Product? Product{get;set;}[Required]public int Quantity{get;set;}[Column(TypeName="decimal(18,2)")]public decimal ImportPrice{get;set;}}
